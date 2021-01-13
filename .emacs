768bd052e3eaafd45f8819effab3d422ded36b7d
David <dwoodward@salesforce.com>
2021-01-13 15:27:23 -0700

    A clean setup with some typescript stuff and just a few helpful libraries.

--- .emacs
+++ .emacs
@@ -1,21 +1,54 @@
-;;; This was installed by package-install.el.
-;;; This provides support for the package system and
-;;; interfacing with ELPA, the package archive.
-;;; Move this code earlier if you want to reference
-;;; packages in your .emacs.
-(when
-    (load
-     (expand-file-name "~/.emacs.d/elpa/package.el"))
-  (package-initialize))
+;; In Emacs 27 package-initialize has already been called in early-init
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
-(add-to-list 'load-path "~/.site-lisp")
-;; Configuration of emacs itself
-(load "emacs-configs")
-;; My ELisp
-(load "daves-plugins")
-;; Extra/modified modes
-(load "extra-modes")
-;; ELisp that enhances emacs
-(load "emacs-extensions")
-;; One emacs... many clients.
-(server-start)
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  (company-mode +1))
+
+(use-package tide
+  :ensure t
+  :after (typescript-mode company flycheck)
+  :hook ((typescript-mode . tide-setup)
+         (typescript-mode . tide-hl-identifier-mode)
+         (before-save . tide-format-before-save)))
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
+
+;; enable typescript-tslint checker
+;; (flycheck-add-mode 'typescript-tslint 'web-mode)
+
+(use-package projectile
+  :ensure t
+  :init
+  (projectile-mode +1)
+  :bind (:map projectile-mode-map
+	      ("C-c p" . projectile-command-map)))
+
+(use-package treemacs-projectile
+  :after treemacs projectile
+  :ensure t)
+
+(use-package treemacs-magit
+  :after treemacs magit
+  :ensure t)
+
+(when (equal system-type 'darwin)
+  (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
+  (push "/usr/local/bin" exec-path))
+
+(setq custom-file "~/.emacs.d/emacs-custom.el")
+(load-file custom-file)
+
+(toggle-scroll-bar -1)
---
+++ emacs-custom.el
@@ -0,0 +1,59 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   (vector "#ffffff" "#c82829" "#718c00" "#eab700" "#4271ae" "#8959a8" "#3e999f" "#4d4d4c"))
+ '(beacon-color "#c82829")
+ '(company-idle-delay 0.1)
+ '(custom-enabled-themes '(sanityinc-tomorrow-day))
+ '(custom-file "~/.emacs.d/emacs-custom.el")
+ '(custom-safe-themes
+   '("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default))
+ '(fci-rule-color "#d6d6d6")
+ '(flycheck-color-mode-line-face-to-color 'mode-line-buffer-id)
+ '(frame-background-mode nil)
+ '(icomplete-mode nil)
+ '(ivy-mode t)
+ '(line-number-mode t)
+ '(ns-command-modifier 'meta)
+ '(package-selected-packages
+   '(ivy color-theme-sanityinc-tomorrow treemacs-magit treemacs-projectile treemacs magit projectile paredit use-package web-mode eldoc company tide))
+ '(tab-width 4)
+ '(tide-format-options '(convertTabsToSpaces nil))
+ '(tool-bar-mode nil)
+ '(treemacs-fringe-indicator-mode '##)
+ '(treemacs-git-mode '##)
+ '(treemacs-no-png-images t)
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   '((20 . "#c82829")
+	 (40 . "#f5871f")
+	 (60 . "#eab700")
+	 (80 . "#718c00")
+	 (100 . "#3e999f")
+	 (120 . "#4271ae")
+	 (140 . "#8959a8")
+	 (160 . "#c82829")
+	 (180 . "#f5871f")
+	 (200 . "#eab700")
+	 (220 . "#718c00")
+	 (240 . "#3e999f")
+	 (260 . "#4271ae")
+	 (280 . "#8959a8")
+	 (300 . "#c82829")
+	 (320 . "#f5871f")
+	 (340 . "#eab700")
+	 (360 . "#718c00")))
+ '(vc-annotate-very-old-color nil)
+ '(window-divider-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#ffffff" :foreground "#4d4d4c" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "nil" :family "Source Code Pro"))))
+ '(fringe ((t (:foreground "#969896")))))
