7216ca70a4dcdbe628bdec2281fc972f4d9416e0
David <dwoodward@salesforce.com>
2021-03-18 09:33:26 -0600

    Adds rust-mode. Tweaks theme. Adds org-mode settings. Other misc settings

--- emacs-custom.el
+++ emacs-custom.el
@@ -6,10 +6,11 @@
  '(ansi-color-faces-vector
    [default bold shadow italic underline bold bold-italic bold])
  '(ansi-color-names-vector
-   (vector "#ffffff" "#c82829" "#718c00" "#eab700" "#4271ae" "#8959a8" "#3e999f" "#4d4d4c"))
+   (vector "#1d1f21" "#cc6666" "#b5bd68" "#f0c674" "#81a2be" "#b294bb" "#8abeb7" "#c5c8c6"))
+ '(backup-directory-alist '(("." . "~/.emacs.d/file-backups")))
  '(beacon-color "#c82829")
  '(company-idle-delay 0.1)
- '(custom-enabled-themes '(sanityinc-tomorrow-day))
+ '(custom-enabled-themes '(leuven))
  '(custom-file "~/.emacs.d/emacs-custom.el")
  '(custom-safe-themes
    '("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default))
@@ -19,11 +20,18 @@
  '(icomplete-mode nil)
  '(ivy-mode t)
  '(line-number-mode t)
+ '(neo-theme 'icons)
+ '(neo-window-width 30)
  '(ns-command-modifier 'meta)
+ '(org-agenda-files '("~/Desktop/2021-01-notes.org"))
+ '(org-log-done 'time)
+ '(org-todo-keywords '((sequence "TODO" "CANCELLED" "DONE")))
  '(package-selected-packages
-   '(ivy color-theme-sanityinc-tomorrow treemacs-magit treemacs-projectile treemacs magit projectile paredit use-package web-mode eldoc company tide))
+   '(prettier-js rust-mode ivy color-theme-sanityinc-tomorrow treemacs-magit treemacs-projectile treemacs magit projectile paredit use-package web-mode eldoc company tide))
+ '(projectile-mode t nil (projectile))
+ '(rust-format-on-save t)
  '(tab-width 4)
- '(tide-format-options '(convertTabsToSpaces nil))
+ '(tide-format-options nil)
  '(tool-bar-mode nil)
  '(treemacs-fringe-indicator-mode '##)
  '(treemacs-git-mode '##)
@@ -55,5 +63,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#ffffff" :foreground "#4d4d4c" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "nil" :family "Source Code Pro"))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "nil" :family "Source Code Pro"))))
  '(fringe ((t (:foreground "#969896")))))
