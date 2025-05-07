e96c5bcf127de9106e0bb6ee1a0b4f0f9687fdb3
David Woodward <dave.woodward@aed.pro>
2025-05-07 15:51:50 -0600

    Updates init to add js lsp.
    Also adds a custom.el file that was created when setting up ellama. This
    probably can be removed but no time to look into it at the moment.

--- init.el
+++ init.el
@@ -140,8 +140,9 @@
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
        json                ; At least it ain't XML
-       (java +lsp)       ; the poster child for carpal tunnel syndrome
-       javascript          ; all(hope(abandon(ye(who(enter(here))))))
+       (java +lsp)         ; the poster child for carpal tunnel syndrome
+       (javascript         ; all(hope(abandon(ye(who(enter(here))))))
+        +lsp +typescript +rjsx +tide)
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        ;;latex             ; writing papers in Emacs has never been so fun
@@ -157,12 +158,13 @@
        ;;plantuml          ; diagrams for confusing people more
        ;;graphviz          ; diagrams for confusing yourself even more
        ;;purescript        ; javascript, but functional
-       (python +lsp +pyright)      ; beautiful is better than ugly
+       (python             ; beautiful is better than ugly
+        +lsp +pyright)
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
        rest                ; Emacs as a REST client
-       ;;rst               ; ReST in peace
+       rst                 ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;;(rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
